66f5300120e81dbc6b58bdffec57b4b71060e3cf
DrInfinite <naparajith@gmail.com>
2021-09-18 09:11:29 +0530

    18/09/2021 Commit

--- Test.java
+++ Test.java
@@ -1,4 +1,4 @@
-/*class Test {
+class Test {
     public static void main(String[] args) {
         //String binary[] = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", 
         //"1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
@@ -17,10 +17,10 @@
         System.out.println(g);
         System.out.println(h);
     }
-}*/
-
-class Test {
-    public static void main(String[] args) {
-        
-    }
 }
+
+/* class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+} */
